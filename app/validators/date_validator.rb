d5e63b08cdfbab1a86a701264aa7552f988e2528
James Doyley <jdoyley@gmail.com>
2015-06-26 14:55:44 +0100

    Add some tests

--- app/validators/date_validator.rb
+++ app/validators/date_validator.rb
@@ -2,6 +2,7 @@ class DateValidator < ActiveModel::Validator
   def validate(record)
     start_date = record.send(options[:start_date])
     end_date = record.send(options[:end_date])
+    return if start_date.blank? or end_date.blank? # just return and let the presence validation happen
     record.errors[options[:start_date]] << 'Must be before end date' unless start_date < end_date
   end
 
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:start_date)
+  should validate_presence_of(:end_date)
+
+  context "an event with a start date before end date" do
+    setup do
+      @event = Event.new(start_date: Date.today, end_date: Date.today + 1)
+    end
+
+    should "pass validation" do
+      assert @event.valid?
+    end
+  end
+
+  context "an event with a start date and end date the same" do
+    setup do
+      @event = Event.new(start_date: Date.today, end_date: Date.today)
+    end
+
+    should "fail validation" do
+      refute @event.valid?
+    end
+  end
+
+  context "an event with a start date after the end date" do
+    setup do
+      @event = Event.new(start_date: Date.today + 1, end_date: Date.today)
+    end
+
+    should "fail validation" do
+      refute @event.valid?
+    end
+  end
 end
